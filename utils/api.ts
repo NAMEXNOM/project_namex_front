6ffdd1f7d153babbec1838f27ebdcec582b6e573
unknown <ramon.anchondo@gmail.com>
2026-06-28 16:29:08 -0600

    Fix: apis

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 // utils/api.ts
 declare const process: any;
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api/docs';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api/docs#';
 
 export const fetchCustom = async (endpoint: string, options: RequestInit = {}) => {
     return fetch(`${API_URL}${endpoint}`, options);
